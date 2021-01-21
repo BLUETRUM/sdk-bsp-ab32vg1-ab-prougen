b41f57eda32378c5d937cfde33c3f9797b219226
greedyhao <hao_kr@163.com>
2021-01-21 16:04:48 +0800

    更新版本信息

--- rtt_default_project_0/libraries/hal_drivers/drv_sdio.h
+++ rtt_default_project_0/libraries/hal_drivers/drv_sdio.h
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2006-2020, RT-Thread Development Team
- *
+ * Copyright (c) 2020-2021, Bluetrum Development Team
+ * 
  * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
  * Date           Author            Notes
- * 2020-11-30     greedyhao         first version
+ * 2021-01-20     greedyhao         first version
  */
 
 #ifndef DEV_SDIO_H__
