93f6c7af0cdb0bdb6ea5a750c593526d139c4bad
greedyhao <hao_kr@163.com>
2021-04-09 17:47:07 +0800

    更新配置

--- rtt_default_project_0/rtconfig.h
+++ rtt_default_project_0/rtconfig.h
@@ -72,26 +72,6 @@
 
 /* Device virtual file system */
 
-#define RT_USING_DFS
-#define DFS_USING_WORKDIR
-#define DFS_FILESYSTEMS_MAX 2
-#define DFS_FILESYSTEM_TYPES_MAX 2
-#define DFS_FD_MAX 16
-#define RT_USING_DFS_ELMFAT
-
-/* elm-chan's FatFs, Generic FAT Filesystem Module */
-
-#define RT_DFS_ELM_CODE_PAGE 437
-#define RT_DFS_ELM_WORD_ACCESS
-#define RT_DFS_ELM_USE_LFN_3
-#define RT_DFS_ELM_USE_LFN 3
-#define RT_DFS_ELM_LFN_UNICODE_0
-#define RT_DFS_ELM_LFN_UNICODE 0
-#define RT_DFS_ELM_MAX_LFN 255
-#define RT_DFS_ELM_DRIVES 2
-#define RT_DFS_ELM_MAX_SECTOR_SIZE 512
-#define RT_DFS_ELM_REENTRANT
-#define RT_USING_DFS_DEVFS
 
 /* Device Drivers */
 
@@ -99,24 +79,7 @@
 #define RT_PIPE_BUFSZ 512
 #define RT_USING_SERIAL
 #define RT_SERIAL_RB_BUFSZ 64
-#define RT_USING_HWTIMER
-#define RT_USING_I2C
-#define RT_USING_I2C_BITOPS
 #define RT_USING_PIN
-#define RT_USING_ADC
-#define RT_USING_PWM
-#define RT_USING_RTC
-#define RT_USING_SDIO
-#define RT_SDIO_STACK_SIZE 512
-#define RT_SDIO_THREAD_PRIORITY 15
-#define RT_MMCSD_STACK_SIZE 1024
-#define RT_MMCSD_THREAD_PREORITY 22
-#define RT_MMCSD_MAX_PARTITION 16
-#define RT_USING_WDT
-#define RT_USING_AUDIO
-#define RT_AUDIO_REPLAY_MP_BLOCK_SIZE 4096
-#define RT_AUDIO_REPLAY_MP_BLOCK_COUNT 2
-#define RT_AUDIO_RECORD_PIPE_SIZE 2048
 
 /* Using USB */
 
@@ -124,7 +87,6 @@
 /* POSIX layer and C standard library */
 
 #define RT_USING_LIBC
-#define RT_USING_POSIX
 
 /* Network */
 
@@ -182,7 +144,6 @@
 
 /* peripheral libraries and drivers */
 
-#define SDIO_MAX_FREQ 24000000
 
 /* AI packages */
 
@@ -200,31 +161,11 @@
 
 /* Onboard Peripheral Drivers */
 
-#define BSP_USING_AUDIO
-#define BSP_USING_AUDIO_PLAY
-#define BSP_USING_SDCARD
 
 /* On-chip Peripheral Drivers */
 
 #define BSP_USING_UART
 #define BSP_USING_UART0
-#define BSP_USING_SDIO
-#define BSP_USING_I2C1
-
-/* Notice: PE3 --> 16; PE2 --> 15 */
-
-#define BSP_I2C1_SCL_PIN 16
-#define BSP_I2C1_SDA_PIN 15
-#define BSP_USING_PWM
-#define BSP_USING_T5_PWM
-#define BSP_USING_T5_PWM0
-#define BSP_USING_WDT
-#define BSP_USING_TIM
-#define BSP_USING_TIM1
-#define BSP_USING_ONCHIP_RTC
-#define RTC_USING_INTERNAL_CLK
-#define BSP_USING_ADC
-#define BSP_USING_ADC0
 
 /* Board extended module Drivers */
 
